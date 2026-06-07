9f975f9034849b2d3b3e325d39284cd650088b82
Nia Cele <st10439898@vcconnect.edu.za>
2026-06-07 12:10:24 +0200

    Update ClientApiService
     - made reference to the correct view model
    - corrected typo in GetClientAsync()

--- Global Logistics Management System - ST10439898/Global Logistics Management System - ST10439898/Services/ClientApiService.cs
+++ Global Logistics Management System - ST10439898/Global Logistics Management System - ST10439898/Services/ClientApiService.cs
@@ -14,21 +14,21 @@ namespace Global_Logistics_Management_System___ST10439898.Services
         }
 
         //GET - all clients
-        public async Task<List<Client>> GetClientAsync()
+        public async Task<List<ClientViewModel>> GetClientAsync()
         {
             //send a GET request to the API
-            var clients = await _httpclient.GetAllClientsAsync<List<Client>>(""); //retrieves JSON data and converts it to a C# list
-
-            return clients ?? new List<Client>();
+            var clients = await _httpclient.GetFromJsonAsync<List<ClientViewModel>>("");
+            
+            return clients ?? new List<ClientViewModel>();
         }
 
         //GET - specific client by ID
-        public async Task<Client?> GetClientByIdAsyc (int id)
+        public async Task<ClientViewModel?> GetClientByIdAsyc (int id)
         {
             try
             {
                 //append ID onto URL path 
-                return await _httpclient.GetFromJsonAsync<Client>($"{id}");
+                return await _httpclient.GetFromJsonAsync<ClientViewModel>($"{id}");
 
             }
 
@@ -40,7 +40,7 @@ namespace Global_Logistics_Management_System___ST10439898.Services
         }
 
         //CREATE - new client
-        public async Task<HttpResponseMessage> UpdateClientAsync (int id, Client client) //return type is HttpResponseMessage rather than Client object - method can display correct alerts
+        public async Task<HttpResponseMessage> CreateClientAsync(int id, ClientViewModel client) //return type is HttpResponseMessage rather than Client object - method can display correct alerts
         {
             //converts C# to JSON so that the API can understand
             return await _httpclient.PostAsJsonAsync("", client);
@@ -48,7 +48,7 @@ namespace Global_Logistics_Management_System___ST10439898.Services
         }
 
         //UPDATE - update client
-        public async Task<HttpResponseMessage> UpdateClientAsync(int id, ClientApiService client)
+        public async Task<HttpResponseMessage> UpdateClientAsync(int id, ClientViewModel client)
         {
             //converts C# to JSON so that the API can understand
             return await _httpclient.PutAsJsonAsync($"{id}", client);
