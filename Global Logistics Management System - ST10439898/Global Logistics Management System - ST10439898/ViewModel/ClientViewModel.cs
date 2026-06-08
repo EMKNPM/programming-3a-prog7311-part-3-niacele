60d10232e3ea24c6b9595c03b144de6e061a4fe4
Nia Cele <st10439898@vcconnect.edu.za>
2026-06-08 08:27:51 +0200

    Update ClientViewModel

--- Global Logistics Management System - ST10439898/Global Logistics Management System - ST10439898/ViewModel/ClientViewModel.cs
+++ Global Logistics Management System - ST10439898/Global Logistics Management System - ST10439898/ViewModel/ClientViewModel.cs
@@ -4,14 +4,14 @@ namespace Global_Logistics_Management_System___ST10439898.ViewModels
 {
     public class ClientViewModel
     {
-        public int cientId { get; set;  }
-        [Display(name = "Client Name")]
+        public int clientId { get; set;  }
+        [Display(Name = "Client Name")]
         public string clientName { get; set; }
-        [Display(name = "Email Address")]
+        [Display(Name = "Email Address")]
         public string clientEmail { get; set; }
-        [Display(name = "Logistics Region Zone")]
+        [Display(Name = "Logistics Region Zone")]
         public string clientRegion { get; set; }
-        [Display(name = "Company Name")]
+        [Display(Name = "Company Name")]
         public string companyName { get; set; }
 
         //no relationship attributes because this is handled in the API
